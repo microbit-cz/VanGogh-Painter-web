38b0908b21c4e88dcccd4f3cbe176142af1e938a
Jan Holý <jan.holy.021@pslib.cz>
2024-11-08 05:35:01 +0100

    react router nearly functional (i think)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-/*
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import {Welcome} from "./pages/Welcome.tsx";
 import {AboutUs} from "./pages/AboutUs.tsx";
 import {Painter} from "./pages/Painter.tsx";
-*/
-//import {Header} from "./components/Header.tsx";
 
-import {ScaleSetting} from "./components/ScaleSetting.tsx";
 
 function App() {
-    /*
+
     const router = createBrowserRouter(
         createRoutesFromElements(
             <>
@@ -19,13 +15,10 @@ function App() {
             </>
         ), {basename: "/MP2024-25_Holy-Jan_VanGogh-Painter"}
     )
-    */
+
 
     return (
-        //<RouterProvider router={router} />
-        <>
-            <ScaleSetting />
-        </>
+        <RouterProvider router={router} />
     )
 }
 
