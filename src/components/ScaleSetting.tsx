bf290c6018cf05bb5a957cff8deba608ac2d52f4
hongolem <hongolem@seznam.cz>
2025-01-22 04:28:50 +0100

    some details, SVG path, some resposive layout problems

--- src/components/ScaleSetting.tsx
+++ src/components/ScaleSetting.tsx
@@ -1,15 +1,29 @@
 import {Icon, IconVariant} from "./Icon.tsx";
 import Styles from "./ScaleSetting.module.css";
-import {FC} from "react";
+import {FC, useState} from "react";
 
 interface ScaleSettingProps {
     onScaleChange: (scale: number) => void;
 }
 
 export const ScaleSetting: FC<ScaleSettingProps> = ({ onScaleChange }) => {
+    const [scale, setScale] = useState(1);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newScale = parseFloat(event.target.value);
+        const newScale = Math.round(parseFloat(event.target.value) * 10) / 10;
+        setScale(newScale);
+        onScaleChange(newScale);
+    };
+
+    const handleIncrease = () => {
+        const newScale = Math.min(scale + 0.1, 10);
+        setScale(newScale);
+        onScaleChange(newScale);
+    };
+
+    const handleDecrease = () => {
+        const newScale = Math.max(scale - 0.1, 0.1);
+        setScale(newScale);
         onScaleChange(newScale);
     };
 
@@ -17,12 +31,14 @@ export const ScaleSetting: FC<ScaleSettingProps> = ({ onScaleChange }) => {
         <div className={Styles["scaleSetting__container"]}>
             <p className={Styles["scaleSetting__title"]}>Real size multiplier:</p>
             <div className={Styles["scaleSetting__inputContainer"]}>
-                <button className={Styles["scaleSetting__button"]}><Icon variant={IconVariant.MINUS} size={1} /></button>
+                <button onClick={handleDecrease} className={Styles["scaleSetting__button"]}><Icon variant={IconVariant.MINUS} size={1} /></button>
                 <div className={Styles["scaleSetting__inputNumberContainer"]}>
                     <span className={Styles["scaleSetting__unit"]}>×</span>
-                    <input className={Styles["scaleSetting__input"]} type="number" id="scale" name="scale" min="0.1" max="10" step="0.1" defaultValue="1" onChange={handleInputChange}/>
+                    <input className={Styles["scaleSetting__input"]} type="number" id="scale" name="scale" min="0.1"
+                           max="10" step="0.1" value={scale} onChange={handleInputChange}/>
                 </div>
-                <button className={Styles["scaleSetting__button"]}><Icon variant={IconVariant.PLUS} size={1}/></button>
+                <button onClick={handleIncrease} className={Styles["scaleSetting__button"]}><Icon
+                    variant={IconVariant.PLUS} size={1}/></button>
             </div>
         </div>
     );
