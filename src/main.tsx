d51f51770327c156f43f288afa5e62c6e564a3fd
hongolem <hongolem@seznam.cz>
2025-04-09 22:11:28 +0200

    fix2

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {PainterProvider} from "./providers/PainterProvider.tsx";
-import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <PainterProvider>
             <App />
         </PainterProvider>
-        <Analytics />
     </StrictMode>,
 )
